93249f15ae5517c9fe87c91d15bbf960d5072ec7
Matthew J. Thompson <thompson.m.j@outlook.com>
2019-03-21 12:04:06 -0400

    Change uniformsample variable names

--- src/uniformsample.m
+++ src/uniformsample.m
@@ -1,17 +1,17 @@
-function xOut = uniformsample(xIn)
-% UNIFORMSAMPLE resamples a 1D array to a constant rate. Each column of a
+function AOut = uniformsample(AIn,nSamples)
+% UNIFORMSAMPLE resamples an array to a constant rate. Each column of a
 % matrix is treated as a separate channel.
 
-NSAMPLES = 1000;
+nSamples = 1000;
 
-nSignals = size(xIn,2);
+nCol = size(AIn,2);
 
-x = (1:numel(xIn(:,1)))';
-xp = linspace(x(1), x(end), NSAMPLES);
-xOut = zeros(NSAMPLES,nSignals);
+x = (1:numel(AIn(:,1)))';
+xp = linspace(x(1), x(end), nSamples);
+AOut = zeros(nSamples,nCol);
 
-for iSignal = 1:nSignals
-    xOut(:,iSignal) = interp1(x, xIn(:,iSignal), xp); % Default: linear inerp.
+for iCol = 1:nCol
+    AOut(:,iCol) = interp1(x, AIn(:,iCol), xp); % Default: linear inerp.
 end
 
 end
