a51a33117475a5c80e1514bc2263da8011f18b31
Matthew J. Thompson <thompson.m.j@outlook.com>
2019-07-22 15:07:29 -0400

    Simplify smoothrawdata to smooth just one profile per call

--- src/smoothrawdata.m
+++ src/smoothrawdata.m
@@ -1,23 +1,15 @@
-function smDat = smoothrawdata(RawDat,x,method,varargin)
+function smDat = smoothrawdata(rawDat,method,win)
 %SMOOTHRAWDATA Smooth all profiles within a cryosection.
 %
-% Input:
-%   > RawDat: raw columnar profile data to be smoothed.
-%       Either a struct with target fieldnames or a single array.
-%   > RawDat: Struct containing raw columnar data from each channel (target)
-%   in this cryosection to be aligned.
-%       Format: RawDat.Tgt1, RawDat.Tgt2, ...
-%       Fields: double, NxM (N = smapled points, M = no. optical sections)
-%   > x: Raw circumferencial position data for this cryosection. Used for
-%   setting window size.
-%       double, Nx1 (N = sampled points)
-%   > method: smoothing method.
-%       'loess' or 'sgolay'
+% Inputs:
+%   > rawDat: Raw columnar profile data to be smoothed.
+%   > method: Smoothing method (MATLAB built-in).
 % Output:
-%   > smDat: Smooth data struct complimentary to RawDat.
+%   > smDat: Smooth data complimentary to rawDat.
 
 %% Input validation.
-% assert( isstruct(RawDat) |  )
+dims = size(rawDat);
+assert( dims(1) > 1 && dims(2) == 1 ); % Must be column matrix.
 assert( isequal(method, 'loess') | ...
         isequal(method, 'sgolay') | ...
         isequal(method, 'movmean') | ...
@@ -27,72 +19,17 @@ assert( isequal(method, 'loess') | ...
         isequal(method, 'rloess') | ...
         isequal(method, 'rlowess') )
 
-%% Setup
-T = fieldnames(RawDat);
-nTgts = size(T,1);
+%% Execute smoothing.
+appendSize = ceil(win/2);
+startPortion = repmat(rawDat(1),appendSize,1);
+endPortion = repmat(rawDat(end),appendSize,1);
+rawDatAppended = [startPortion; rawDat; endPortion];
+nPtsAppended = numel(rawDatAppended);
 
-% Adjust smoothing window size based on image resolution.
-% Adequate window size determined by eye.
-switch numel(varargin)
-    case 0
-        if mean(diff(x)) > 0.302 && mean(diff(x)) < 0.322   
-            % Corresponds to 1024 x 1024 image resolution (0.312).
-            win = 160;
-        elseif mean(diff(x)) > 0.614 && mean(diff(x)) < 0.634
-            % Corresponds to 512 x 512 image resolution (0.624).
-            win = 80;
-        else 
-            warning('Unexpected resolution. Scaling window size accordingly');
-            d = mean(diff(x));
-            win = round( d*(-801.2821) + 750 ); % Derived from above settings.
-        end
-    case 1
-        win = varargin{1};
-end
+smDatAppended = smoothdata(rawDatAppended,method,win);
 
-
-%% Execute smoothing on each optical section.
-for iTgt = 1:nTgts
-    
-    %%%RawDatcell{iTgt} => RawDat.(T{iTgt})
-    profileSet = RawDat.(T{iTgt});
-%     profileSet = RawDatcell{iTgt};
-    
-    nZ = size(profileSet,2);
-    
-    %%% smDatcell{iTgt} => smDat.(T{iTgt})
-    smDat.(T{iTgt}) = zeros(size(profileSet));
-%     smDatcell{iTgt} = zeros(size(profileSet));
-    
-    for iZ = 1:nZ
-       profile = RawDat.(T{iTgt})(:,iZ);
-       nPts = numel(profile);
-       appendSize = ceil(win/2);
-       startPortion = profile( 1 : appendSize ); 
-       endPortion = profile( nPts-appendSize : nPts );
-       profileAppended = [endPortion; profile; startPortion];
-       nPtsAppended = numel(profileAppended);
-       
-       smProfileAppended = smoothdata(profileAppended,method,win);
-       
-       smProfileUnappended = smProfileAppended( appendSize+1 : ...
-                                                nPtsAppended - (appendSize+1) );
-       smDat.(T{iTgt})(:,iZ) = [smProfileUnappended];
-       
-    end
-    
-%     % Normalize smoothed optical sections on this channel.
-%     normOut = normdat( smDatcell{iTgt} );
-%     
-%     %%%meanDatCell{iTgt} => meanDat.(T.{iTgt})
-%     meanDatcell{iTgt} = mean(normOut.ChiSq,2);
-
-end
-
-% % Parse data into structures by target as fieldnames.
-% for iTgt = 1:nTgts
-%     smDat.(T{iTgt}) = smDatcell{iTgt};
-%     meanDat.(T{iTgt}) = meanDatcell{iTgt};
-% end
+smDatUnappended = ... 
+    smDatAppended( appendSize+1 : nPtsAppended - (appendSize+1) );
+smDat = [smDatUnappended];
 
 end
