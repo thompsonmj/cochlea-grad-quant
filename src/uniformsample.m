3739581dfdb023c72a6ea8a510ab77254b273517
Matthew J. Thompson <thompson.m.j@outlook.com>
2020-09-02 11:02:22 -0400

    I/O documentation.

--- src/uniformsample.m
+++ src/uniformsample.m
@@ -1,6 +1,12 @@
 function AOut = uniformsample(AIn,nSamples)
 % UNIFORMSAMPLE resamples an array to a constant rate. Each column of a
 % matrix is treated as a separate channel.
+%
+% Inputs:
+%   > AIn: array (columnar)
+%   > nSamples: number of samples to interpolate from AIn
+% Outputs:
+%   > AOut: Resampled output containing nSamples points
 
 nCol = size(AIn,2);
 
